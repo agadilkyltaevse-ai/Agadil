75d7ab9c964122c53d2de9f1cfbb04fd133ae846
Агадил <agadilkyltaevse@gmail.com>
2026-04-20 14:29:10 +0500

    Add headless mode to chrome options for linux compatibility

--- conftest.py
+++ conftest.py
@@ -7,6 +7,7 @@ from selenium.webdriver.chrome.options import Options
 def driver():
     options = Options()
     options.add_argument("--start-maximized")
+    options.add_argument("--headless=new")
     options.add_argument("--disable-blink-features=AutomationControlled")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
